dc1b715ae26543e507f6e8e262168bcf64ea653e
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2025-03-10 22:08:25 +0100

    Add hamburger-react for responsive navbar; update Navbar component to use hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Sling as Hamburger } from "hamburger-react";
 import "../App.css";
+
 const Navigation = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <Navbar
       bg="dark"
@@ -9,12 +13,33 @@ const Navigation = () => {
       expand="lg">
       <Container>
         <Navbar.Brand href="#home">Mein CV</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <div className="custom-toggle">
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            color="#fff"
+          />
+        </div>
+
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className={isOpen ? "show" : ""}>
           <Nav className="ml-auto">
-            <Nav.Link href="#about">Über mich</Nav.Link>
-            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="#contact">Kontakt</Nav.Link>
+            <Nav.Link
+              href="#about"
+              onClick={() => setOpen(false)}>
+              Über mich
+            </Nav.Link>
+            <Nav.Link
+              href="#portfolio"
+              onClick={() => setOpen(false)}>
+              Portfolio
+            </Nav.Link>
+            <Nav.Link
+              href="#contact"
+              onClick={() => setOpen(false)}>
+              Kontakt
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
